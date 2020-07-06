143e9c081e5484201d3aa1f1d27e34d2f889413b
Viviane Gomes Araujo <viviane.gomes@timeguilu.com.br>
2020-07-05 22:41:52 -0300

    v0.0.3 register page styled

--- web/src/pages/register/index.js
+++ web/src/pages/register/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo3x.png';
 
 class Register extends Component {
     render() { return(
         <Container>
-            <div>
-                <p>deu bom</p>
+            <div className='containerItems'>
+                <div className='logoContainer'>
+                    <img className='logoImg' src={logo} alt='logo Be The Hero'/>
+                    <h2 className='title'>Cadastro</h2>
+                    <p className='catchphrase'>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrar os casos da sua ONG.</p>
+                    <Link to='/' className='homeLink'><small className='leftArrow'>🡐</small> Já possuo cadastro</Link>
+                </div>
+                <div>
+                    <form className='formularyContainer'>
+                        <input className='inputStyle' type='text' placeholder='Nome da ONG'/>
+                        <input className='inputStyle' type='email' placeholder='E-mail'/>
+                        <input className='inputStyle' type='tel' pattern="\(\d\d\)\ \d\d\d\d\d\-\d\d\d\d\" placeholder='WhatsApp'/>
+                        <div>
+                            <input id='city' className='inputStyle' type='text' placeholder='Cidade'/>
+                            <input id='uf' className='inputStyle' type='text' placeholder='UF'/>
+                        </div>
+                        <button type='submit' className='redButton'>Cadastrar</button>
+                    </form>
+                </div>
             </div>
         </Container>
     );
--- web/src/pages/register/styles.js
+++ web/src/pages/register/styles.js
@@ -1,5 +1,105 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background:red;
+    height: 450px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 150px;
+    margin-left: 18%;
+    width: 60%;
+    box-shadow: 0px 0px 50px #BEBEBE;
+    border-radius: 4px;
+
+    .containerItems {
+        display: flex;
+        padding-right: 80px;
+        padding-left: 80px;
+    }
+
+    .logoContainer {
+        padding-right: 50px;
+    }
+
+    .logoImg {
+        max-width:200px;
+        max-height:150px;
+        width: auto;
+        height: auto;
+        margin-bottom: 25px;
+
+    }
+
+    .catchphrase {
+        font-size: 14px;
+        line-height: 25px;
+        color: #9D9DA3;
+        font-weight: bold;
+        margin-bottom: 30px;
+    }
+
+    .homeLink {
+        color:#4D4E53;
+        cursor: pointer;
+        font-weight: bold;
+        font-size:14px;
+        text-decoration: none;
+        :hover {
+            color: #232426;
+            text-decoration: none;
+        }
+
+        small {
+            color: #E02142;
+            font-weight: bold;
+        }
+    }
+
+    .formularyContainer {
+        display: flex;
+        flex-direction: column;
+
+        div {
+            display: flex;
+        }
+
+        #city {
+            width: 280px;
+            margin-right: 10px;
+        }
+
+        #uf {
+            width: 40px;
+        }
+    }
+
+    .inputStyle{
+        width: 350px;
+        height: 43px;
+        border:none;
+        border-radius: 6px;
+        padding-left: 18px;
+        margin-bottom: 10px;
+    }
+
+    .inputStyle::placeholder {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    .redButton {
+        background: #E02142;
+        height: 43px;
+        border: none;
+        border-radius: 6px;
+        margin-top: 15px;
+        cursor: pointer;
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+        color: white;
+        width: 367.5px;
+
+        :hover {
+            background: #AD1A33;
+        }
+    }
 `
